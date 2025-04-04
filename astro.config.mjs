144fe6dd8e1fa3664e2d3b177241f5c7cc1357e4
CORBIERE Nolan <nocorb.05@gmail.com>
2025-04-04 20:32:45 +0200

    maj 12

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://LordBaBat.github.io/portfolio-corbierenolan',
+  site: 'https://LordBaBat.github.io',
 })
 
