2ae452cd2c9f968e869c68fde797c2363116a07c
CORBIERE Nolan <nocorb.05@gmail.com>
2025-04-04 22:02:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://corbiere-nolan.github.io',
-  base: '/portefolio/',
+  
   trailingSlash : 'always'
 })
 
